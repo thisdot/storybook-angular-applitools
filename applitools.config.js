4362ef06a52ee475ddd5a43c02ad392cefaf67d3
Bilal Haidar <bhaidar@gmail.com>
2019-02-22 21:09:22 +0200

    upgrade applitools and storybook npm packages

--- applitools.config.js
+++ applitools.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    showLogs: true
+    showLogs: false
 }
--- src/stories/index.stories.ts
+++ src/stories/index.stories.ts
@@ -1,57 +1,46 @@
 import { storiesOf } from '@storybook/angular';
 import { withNotes } from '@storybook/addon-notes';
 import { action } from '@storybook/addon-actions';
-import { ContentEditableComponent } from './../app/components/contenteditable.component';
+import { linkTo } from '@storybook/addon-links';
 
-storiesOf('ContentEditable Component', module)
+import { Welcome, Button } from '@storybook/angular/demo';
+
+storiesOf('Welcome', module).add('to Storybook', () => ({
+  component: Welcome,
+  props: {},
+}));
+
+storiesOf('Button', module)
+  .add('with text', () => ({
+    component: Button,
+    props: {
+      text: 'Hello Button',
+    },
+  }))
   .add(
-    'with yellow background',
-    withNotes('Testing the background color for the editable area and setting it to yellow')(() => ({
-      component: ContentEditableComponent,
+    'with some emoji',
+    withNotes({ text: 'My notes on a button with emojis' })(() => ({
+      component: Button,
       props: {
-        styles: { 'background-color': 'yellow', 'padding': '20px' },
-        ngModel: 'The content goes here',
-        ngModelChange: action('ngModelChange')
-      }
+        text: '😀 😎 👍 💯',
+      },
     }))
   )
   .add(
-    'with red background',
-    withNotes('Testing the background color for the editable area by setting it to red')(() => ({
-      component: ContentEditableComponent,
+    'with some emoji and action',
+    withNotes({ text: 'My notes on a button with emojis' })(() => ({
+      component: Button,
       props: {
-        styles: { 'background-color': 'red', 'color': '#fff', 'padding': '20px' },
-        ngModel: 'The content goes here',
-        ngModelChange: action('ngModelChange')
-      }
+        text: '😀 😎 👍 💯',
+        onClick: action('This was clicked OMG'),
+      },
     }))
   );
-  // .add(
-  //   'with blue background and margin-top 50px',
-  //   withNotes('Testing the background color and margin-top for the editable area by setting it to blue with margin-top of 50px')(() => ({
-  //     component: ContentEditableComponent,
-  //     props: {
-  //       styles: {
-  //         'background-color': 'green',
-  //         'color': '#fff',
-  //         'padding': '20px',
-  //         'margin-top': '50px'
-  //       },
-  //       ngModel: 'The content goes here',
-  //       ngModelChange: action('ngModelChange')
-  //     }
-  //   }))
-  // ).add(
-  //   'with html content',
-  //   withNotes('Testing the content passed to the component by including HTML elements in the content')(() => ({
-  //     component: ContentEditableComponent,
-  //     props: {
-  //       styles: {
-  //         'background-color': '#eee',
-  //         padding: '20px',
-  //       },
-  //       ngModel: '<h1>Header 1</h1>',
-  //       ngModelChange: action('ngModelChange')
-  //     }
-  //   }))
-  // );
+
+storiesOf('Another Button', module).add('button with link to another story', () => ({
+  component: Button,
+  props: {
+    text: 'Go to Welcome Story',
+    onClick: linkTo('Welcome'),
+  },
+}));
